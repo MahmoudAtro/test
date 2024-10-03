dd06d6ee040bcf3e547d4f3d4cf73b5249a6dfc5
MahmoudAtro <mahmoudyatro@gmail.com>
2024-10-03 23:33:59 +0300

    edit flower image

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function showImage() {
-    var image = document.getElementById('image');
-    image.style.display = 'block';
+  var image = document.getElementById("image");
+  image.style.display = "block";
+}
+
+let counter = 0;
+function generateRandomButton() {
+  counter++;
+  if (counter == 5) {
+    showImage();
+    return;
   }
-  
+
+  const containerWidth = 500;
+  const containerHeight = 700;
+
+  const newButton = document.createElement("button");
+  newButton.textContent = "انقري هنا";
+
+  const randomX = Math.random() * (containerWidth - 100);
+  const randomY = Math.random() * (containerHeight - 50);
+
+  newButton.style.left = `${randomX}px`;
+  newButton.style.top = `${randomY}px`;
+
+  document.body.appendChild(newButton);
+
+  newButton.onclick = generateRandomButton;
+}
